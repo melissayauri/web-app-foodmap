401eb538e91e09009215e8cf5eb5f7ed1e464601
melissayauri <yaurimelissa@gmail.com>
2018-02-13 13:43:49 -0500

    funcionalidad para seleccionar el lugar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,143 @@
+/* funcionalidad para la ubicación del usuario y los lugares para comer*/
+var myLocation;
+var initMap = (function() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644},
+    zoom: 14
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      myLocation = pos;
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Esta es tu ubicación');
+      var thisPlace = new google.maps.LatLng(myLocation.lat, myLocation.lng);
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: thisPlace,
+        radius: 1500,
+        type: ['food']
+      }, callback);
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      };
+      function createMarker(thisPlace) {
+        var placeLoc = thisPlace.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: thisPlace.geometry.location
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+          infoWindow.setContent(thisPlace.name);
+          infoWindow.open(map, this);
+        });
+      };
+
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+});
+
+var handleLocationError = (function(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: No deseas la ubicación' :
+    'Error: Your browser doesn\'t support geolocation.');
+});
+
+/* funcionalidad*/
 $(document).ready(function() {
-  /* vista splash por 4 segundos*/
+  var addFilters = (function(element) {
+    var filtersArr = [];
+    for (var i = 0; i < restaurants.length; i++) {
+      for (var n = 0; n < restaurants[i].filters.length; n++) {
+        filtersArr.push(restaurants[i].filters[n]);
+      }
+    }
+    var uniqueFilters = [...new Set(filtersArr)];
+    var filtersFinal = uniqueFilters.sort();
+    for (var a = 0; a < filtersFinal.length; a++) {
+      element.append("<option value='"+filtersFinal[a]+"'>"+filtersFinal[a]+"");
+    };
+    return filtersFinal;
+  });
+  addFilters($('#selection'));
+  /* funcionalidad para el splash*/
   setTimeout(function() {
-    window.location.href = 'views/principal.html';
-  }, 4000);
+    $('.splash').fadeOut();
+    $('.principal-container').fadeIn();
+  }, 1000);
+  setTimeout(function() {
+    initMap();
+  }, 1000);
+
+
+
+  /* funcionalidad para el selector que muestra los lugares */
+  $('#selection').change(function() {
+    $('#place-container').children().remove();
+    var selection = $('select').val();
+    for (var i = 0; i < restaurants.length; i++) {
+      for (var n = 0; n < restaurants[i].filters.length; n++) {
+        if (restaurants[i].filters[n] === selection) {
+          var image = restaurants[i].photo;
+          $('#place-container').append("<div class='col s12 l6 xl6'><div class='place'><p class='title-place'>"+restaurants[i].name+"</p>"+image+"</div></div>");
+        }
+      }
+    };
+
+    /* funcionalidad para que al momento de pasar el cursor se muestre el nombre del lugar*/
+    $('.place').mouseover(function() {
+      $(':nth-child(1)', this).css({'opacity': '1'});
+    });
+
+    $('.place').mouseout(function() {
+      $(':nth-child(1)', this).css({'opacity': '0'});
+    });
+
+    /* inicializando el modal*/
+    $('.modal').modal();
+    /* funcionalidad para adicionar los elementos al modal*/
+    $('.place').click(function() {
+      var place = $(this).children('img').attr('alt');
+      for (var i = 0; i < restaurants.length; i++) {
+        if (place === restaurants[i].name) {
+          $('#name-place').empty();
+          $('#information').empty();
+          $('#name-place').html(restaurants[i].name);
+          $('#ubication').empty();
+          var newName = restaurants[i].name.replace(/ /g, '+');
+          for (var n = 0; n < restaurants[i].address.length; n++) {
+            var newAddress = restaurants[i].address[n].replace(/ /g,'+' );
+            var addressGoogle = newAddress.replace(/,/g, "");
+            $("#ubication").append("<iframe src='https://www.google.com/maps/embed/v1/place?key=AIzaSyAR26jcQ0wriBfIDM3j327c80TqkZjw8-A&q="+addressGoogle+"'allowfullscreen></iframe>");
+            $("#information").append("<p>"+restaurants[i].address[n]+"</p>");
+          }
+          $("#information").append("<p><a href='"+restaurants[i].website+"'>"+restaurants[i].website+"</a></p>");
+        }
+      }
+      $("#modal").modal("open");
+      /**
+      * If the user clicks on the modal overlay, the search resets itself
+      */
+      $(".modal-overlay").click(function() {
+        $("#place-container").children().remove();
+        $("#selection").val(null);
+      });
+    });
+  });
 });
--- js/data.js
+++ js/data.js
@@ -1,64 +1,73 @@
-var data = [
+var restaurants = [
   {
-    name: 'zugarti',
-    photo: '<img src="https://igx.4sqi.net/img/general/200x200/27121880_kA4TBVX3aFlykccL3vFmwwqoZdqEsgSTjA_5B0CSdY8.jpg" alt="">',
-    filter: 'helados',
-    address: 'Av. Alfredo Mendiola 3698 interior N° 78 Independencia',
-    preference: '5 estrellas',
-  },
-  { name: 'palermo',
-    photo: '<img src="https://0.kekantoimg.com/XRJ2GWxMpcxyCfDyOcvb_zMb0ts=/fit-in/600x600/s3.amazonaws.com/kekanto_pics/pics/244/821244.jpg" alt="">',
-    filter: 'helados',
-    address: 'Jr. Huiracocha 1237, Jesús María',
-    preference: '4 estrellas',
+    name: "zugatti",
+      address: ["Jirón Huiracocha 1237, Jesús María 15072, Perú"],
+      filters: ["tortas", "helados"],
+      photo: "<img src='assets/img/zugatti.jpg' alt='zugatti' >",
+      website: "http://runawaysushi.cl"
   },
   {
-    name: 'Delimeli cakes and cupcakes',
-    photo: '<img src="https://static.wixstatic.com/media/76e566_89f4f646e30f7f8f240d5d0edd31fde1.jpg/v1/fill/w_509,h_354,al_c,q_80,usm_0.66_1.00_0.01/76e566_89f4f646e30f7f8f240d5d0edd31fde1.webp" alt="">',
-    filter: 'cupcakes',
-    address: 'Calle Valle Riestra 508 San Isidro',
-    preference: '4 estrellas',
+    name: "palermo",
+    address: ["Av. Universitaria, San Miguel 15088, Perú"],
+    filters: ["tortas", "helados"],
+    photo: "<img src='assets/img/palermo.jpg' alt='palermo'>",
+    website: "http://takeawok.cl/"
   },
   {
-    name: 'miss-cupcakes',
-    photo: '<img src="https://media-cdn.tripadvisor.com/media/photo-s/06/df/1b/de/miss-cupcakes.jpg" alt="">',
-    filter: 'cupcakes',
-    address: 'Av. 28 de julio 810, Miraflores (cerca al cruce con Reducto) ',
-    preference: '5 estrellas'
+    name: "Delimeli cakes and cupcakes",
+    address: ["Avenida General Juan Antonio Pezet 1554, San Isidro 15076, Perú"],
+    filters: ["cupcakes", "tortas"],
+    photo: "<img src='assets/img/deli.webp' alt='Delimeli cakes and cupcakes' >",
+    website: "http://www.wokchile.com/"
   },
   {
-    name: 'dulcería gremil',
-    photo: '<img src="https://d36tnp772eyphs.cloudfront.net/blogs/2/2016/11/Dulceria-Gremil-Sanguitoo.jpg" alt="">',
-    filter: 'sanguito',
-    address: 'Pedro Ruiz 205, Urb. San José – Callao',
-    preference: '5 estrellas'
+    name: "Miss Cupcakes",
+    address: ["Av 28 de Julio 810, Miraflores 15074, Perú"],
+    filters: ["cupcakes"],
+    photo: "<img src='assets/img/misscupcakes.svg' alt='Miss Cupcakes' >",
+    website: "https://www.facebook.com/shaktiveganochile/"
   },
   {
-    name: 'sra. buendia Tejas y Chocotejas',
-    photo: '<img src="https://media-cdn.tripadvisor.com/media/photo-s/08/b2/68/68/photo0jpg.jpg" alt="">',
-    filter: 'chocolate',
-    address: 'Calle Diez Canseco 360 – Miraflores',
-    preference: '3 estrellas'
+    name: "dulcería gremil",
+    address: ["Ruiz Gallo 359-157, Cercado de Lima 07006, Perú"],
+    filters: ["mazamorras", "sanguito", "tortas"],
+    photo: "<img src='assets/img/descarga.jfif' alt='dulcería gremil' >",
+    website: "https://www.facebook.com/Gremil-Dulcer%C3%ADa-383234275065757/?rf=146608605515533"
   },
   {
-    name: 'Dulcería Santa Rosa',
-    photo: '<img src="http://www.generaccion.com/noticia/imagenes/grandes/17_02_2012_17_55_32_1823397345.jpg" alt="">',
-    filter: 'mazamorras',
-    address: 'Jirón Leoncio Prado 471 – Magdalena',
-    preference: '3 estrellas'
+    name: "Dulcería Santa Rosa",
+    address: ["Jirón Leoncio Prado 471, Magdalena del Mar 15086, Perú"],
+    filters: ["mazamorras", "sanguito", "tortas"],
+    photo: "<img src='assets/img/santarosa.jpg' alt='Dulcería Santa Rosa'>",
+    website: "http://www.centralfriends.cl/"
   },
   {
-    name: 'MANJARES – DULCES PERUANOS:',
-    photo: '<img src="https://3.kekantoimg.com/_1LvCqoaZTvCx9YW1b7-Iy5qy1E=/300x300/s3.amazonaws.com/kekanto_pics/pics/231/589231.jpg" alt="">',
-    filter: 'mazamorras',
-    address: 'Av. San Luis 1984 Int. A, San Borja. T. 226-2271.',
-    preference: '4 estrellas'
+    name: "Manjares",
+    address: ["Av San Luis 1984, San Borja 15037, Perú"],
+    filters: ["mazamorras","picarones","suspiro"],
+    photo: "<img src='assets/img/manjares.jpg' alt='Manjares'>",
+    website: "http://www.rockthefood.cl/"
   },
   {
-    name: 'MAGA, MIS SUSPIROS',
-    photo: '<img src="https://3.kekantoimg.com/22rvYl1brpAPJIy2dAyBSKp0IaY=/300x300/s3.amazonaws.com/kekanto_pics/pics/27/793027.jpg" alt="">',
-    filter: 'suspiro',
-    address: 'Av. Benavides 1113, Miraflores (entre Paseo de la República y República de Panamá)',
-    preference: '5 estrellas'
+    name: "Tejas y Chocotejas Buendía",
+    address: ["Avenida Ernesto Diez Canseco 306, Miraflores 15074, Perú"],
+    filters: ["Chocotejas", "galletas"],
+    photo: "<img src='assets/img/buendia.jpg' alt='Tejas y Chocotejas Buendía'>",
+    website: "www.capitancook.cl"
+  },
+  {
+    name: "Dulcería y Pastelería Castañeda",
+    address: ["General Garzón 1281, Jesús María 15072, Perú"],
+    filters: ["pionono", "alfajores"],
+    photo: "<img src='assets/img/castañeda.jfif' alt='Tejas y Chocotejas Buendía'",
+    website: "www.capitancook.cl"
+  },
+  {
+    name: "La Casa del Alfajor",
+    address: ["C.C. Mega Plaza, Av Alfredo Mendiola 3698, Independencia 15311, Perú"],
+    filters: ["pionono", "alfajores"],
+    photo: "<img src='assets/img/alfajor.jfif' alt='La Casa del Alfajor'",
+    website: "www.capitancook.cl"
   }
-];
+
+]
