adad0095ba0a7792280127d86bcb5ff910b80d77
melissayauri <yaurimelissa@gmail.com>
2018-02-18 20:12:59 -0500

    mejorando layout

--- js/data.js
+++ js/data.js
@@ -1,73 +1,72 @@
 var restaurants = [
   {
-    name: "zugatti",
-      address: ["Jirón Huiracocha 1237, Jesús María 15072, Perú"],
-      filters: ["tortas", "helados"],
-      photo: "<img src='assets/img/zugatti.jpg' alt='zugatti' >",
-      website: "http://runawaysushi.cl"
+    name: 'zugatti',
+    address: ['Jirón Huiracocha 1237, Jesús María 15072, Perú'],
+    filters: ['tortas', 'helados'],
+    photo: '<img src="assets/img/zugatti.jpg" alt="zugatti" >',
+    website: 'http://www.zugatti.com/'
   },
   {
-    name: "palermo",
-    address: ["Av. Universitaria, San Miguel 15088, Perú"],
-    filters: ["tortas", "helados"],
-    photo: "<img src='assets/img/palermo.jpg' alt='palermo'>",
-    website: "http://takeawok.cl/"
+    name: 'palermo',
+    address: ['Av. Universitaria, San Miguel 15088, Perú'],
+    filters: ['tortas', 'helados'],
+    photo: '<img src="assets/img/palermo.jpg" alt="palermo">',
+    website: 'https://www.facebook.com/heladeriapalermo/'
   },
   {
-    name: "Delimeli cakes and cupcakes",
-    address: ["Avenida General Juan Antonio Pezet 1554, San Isidro 15076, Perú"],
-    filters: ["cupcakes", "tortas"],
-    photo: "<img src='assets/img/deli.webp' alt='Delimeli cakes and cupcakes' >",
-    website: "http://www.wokchile.com/"
+    name: 'Delimeli cakes and cupcakes',
+    address: ['Avenida General Juan Antonio Pezet 1554, San Isidro 15076, Perú'],
+    filters: ['cupcakes', 'tortas'],
+    photo: '<img src="assets/img/deli.webp" alt="Delimeli cakes and cupcakes" >',
+    website: 'https://www.delimeli.pe/'
   },
   {
-    name: "Miss Cupcakes",
-    address: ["Av 28 de Julio 810, Miraflores 15074, Perú"],
-    filters: ["cupcakes"],
-    photo: "<img src='assets/img/misscupcakes.svg' alt='Miss Cupcakes' >",
-    website: "https://www.facebook.com/shaktiveganochile/"
+    name: 'Miss Cupcakes',
+    address: ['Av 28 de Julio 810, Miraflores 15074, Perú'],
+    filters: ['cupcakes'],
+    photo: '<img src="assets/img/misscupcakes.svg" alt="Miss Cupcakes">',
+    website: 'https://www.facebook.com/misscupcakesperu/'
   },
   {
-    name: "dulcería gremil",
-    address: ["Ruiz Gallo 359-157, Cercado de Lima 07006, Perú"],
-    filters: ["mazamorras", "sanguito", "tortas"],
-    photo: "<img src='assets/img/descarga.jfif' alt='dulcería gremil' >",
-    website: "https://www.facebook.com/Gremil-Dulcer%C3%ADa-383234275065757/?rf=146608605515533"
+    name: 'dulcería gremil',
+    address: ['Ruiz Gallo 359-157, Cercado de Lima 07006, Perú'],
+    filters: ['mazamorras', 'sanguito', 'tortas'],
+    photo: '<img src="assets/img/descarga.jfif" alt="dulcería gremil">',
+    website: 'https://www.facebook.com/Gremil-Dulcer%C3%ADa-383234275065757/?rf=146608605515533'
   },
   {
-    name: "Dulcería Santa Rosa",
-    address: ["Jirón Leoncio Prado 471, Magdalena del Mar 15086, Perú"],
-    filters: ["mazamorras", "sanguito", "tortas"],
-    photo: "<img src='assets/img/santarosa.jpg' alt='Dulcería Santa Rosa'>",
-    website: "http://www.centralfriends.cl/"
+    name: 'Dulcería Santa Rosa',
+    address: ['Jirón Leoncio Prado 471, Magdalena del Mar 15086, Perú'],
+    filters: ['mazamorras', 'sanguito', 'tortas'],
+    photo: '<img src="assets/img/santarosa.jpg" alt="Dulcería Santa Rosa">',
+    website: 'https://www.facebook.com/dulceriasantarosa/'
   },
   {
-    name: "Manjares",
-    address: ["Av San Luis 1984, San Borja 15037, Perú"],
-    filters: ["mazamorras","picarones","suspiro"],
-    photo: "<img src='assets/img/manjares.jpg' alt='Manjares'>",
-    website: "http://www.rockthefood.cl/"
+    name: 'Manjares',
+    address: ['Av San Luis 1984, San Borja 15037, Perú'],
+    filters: ['mazamorras', 'picarones', 'suspiro'],
+    photo: '<img src="assets/img/manjares.jpg" alt="Manjares">',
+    website: 'https://www.facebook.com/Manjares-Dulces-Peruanos-247278248629640/'
   },
   {
-    name: "Tejas y Chocotejas Buendía",
-    address: ["Avenida Ernesto Diez Canseco 306, Miraflores 15074, Perú"],
-    filters: ["Chocotejas", "galletas"],
-    photo: "<img src='assets/img/buendia.jpg' alt='Tejas y Chocotejas Buendía'>",
-    website: "www.capitancook.cl"
+    name: 'Tejas y Chocotejas Buendía',
+    address: ['Avenida Ernesto Diez Canseco 306, Miraflores 15074, Perú'],
+    filters: ['Chocotejas', 'galletas'],
+    photo: '<img src="assets/img/buendia.jpg" alt="Tejas y Chocotejas Buendía">',
+    website: 'https://www.facebook.com/chocotejas.buendia/'
   },
   {
-    name: "Dulcería y Pastelería Castañeda",
-    address: ["General Garzón 1281, Jesús María 15072, Perú"],
-    filters: ["pionono", "alfajores"],
-    photo: "<img src='assets/img/castañeda.jfif' alt='Tejas y Chocotejas Buendía'",
-    website: "www.capitancook.cl"
+    name: 'Dulcería y Pastelería Castañeda',
+    address: ['General Garzón 1281, Jesús María 15072, Perú'],
+    filters: ['pionono', 'alfajores'],
+    photo: '<img src="assets/img/castañeda.jfif" alt="Tejas y Chocotejas Buendía">',
+    website: 'https://www.facebook.com/Dulcer%C3%ADa-y-Pasteler%C3%ADa-Casta%C3%B1eda-1216990945019138/?rf=490195897674306'
   },
   {
-    name: "La Casa del Alfajor",
-    address: ["C.C. Mega Plaza, Av Alfredo Mendiola 3698, Independencia 15311, Perú"],
-    filters: ["pionono", "alfajores"],
-    photo: "<img src='assets/img/alfajor.jfif' alt='La Casa del Alfajor'",
-    website: "www.capitancook.cl"
+    name: 'La Casa del Alfajor',
+    address: ['C.C. Mega Plaza, Av Alfredo Mendiola 3698, Independencia 15311, Perú'],
+    filters: ['pionono', 'alfajores'],
+    photo: '<img src="assets/img/alfajor.jfif" alt="La Casa del Alfajor">',
+    website: 'http://www.lacasadelalfajor.com.pe/'
   }
-
-]
+];
